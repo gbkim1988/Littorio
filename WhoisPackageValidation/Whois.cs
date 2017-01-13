5a02432b49ca285882a136ebe08fcf76894daf48
gbkim1988 <gbkim1988@gmail.com>
2017-01-13 18:27:37 +0900

    update all

---
+++ WhoisPackageValidation/Whois.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoisPackageValidation
+{
+    /// <summary>
+    /// A class to lookup whois information.
+    /// </summary>
+    public class Whois
+    {
+        private const int Whois_Server_Default_PortNumber = 43;
+        private const string Domain_Record_Type = "n";
+        private const string DotCom_Whois_Server = "whois.arin.net";
+
+        /// <summary>
+        /// Retrieves whois information
+        /// </summary>
+        /// <param name="domainName">The registrar or domain or name server whose whois information to be retrieved</param>
+        /// <param name="recordType">The type of record i.e a domain, nameserver or a registrar</param>
+        /// <returns></returns>
+        public static string Lookup(string domainName)
+        {
+            using (TcpClient whoisClient = new TcpClient())
+            {
+                whoisClient.Connect(DotCom_Whois_Server, Whois_Server_Default_PortNumber);
+
+                string domainQuery = Domain_Record_Type + " " + domainName + "\r\n";
+                byte[] domainQueryBytes = Encoding.ASCII.GetBytes(domainQuery.ToCharArray());
+
+                Stream whoisStream = whoisClient.GetStream();
+                whoisStream.Write(domainQueryBytes, 0, domainQueryBytes.Length);
+
+                StreamReader whoisStreamReader = new StreamReader(whoisClient.GetStream(), Encoding.ASCII);
+
+                string streamOutputContent = "";
+                List<string> whoisData = new List<string>();
+                while (null != (streamOutputContent = whoisStreamReader.ReadLine()))
+                {
+                    whoisData.Add(streamOutputContent);
+                }
+
+                whoisClient.Close();
+
+                return String.Join(Environment.NewLine, whoisData);
+            }
+        }
+    }
+}
